4f186c02e89ecfbefb08946be56c3175308ea5f6
nobuaki-shigematsu <n-shigematsu@withearth-system.com>
2019-06-02 13:13:20 +0900

    init

---
+++ precache-manifest.b0d531e43f1f62833c52861430dd126a.js
@@ -0,0 +1,133 @@
+self.__precacheManifest = [
+  {
+    "revision": "dbe05f6038b2bf125551a358bb01e395",
+    "url": "content/images/companylogo256.png"
+  },
+  {
+    "revision": "ce578bde3b0aebc9bf6d08bb267ebab3",
+    "url": "index.html"
+  },
+  {
+    "revision": "aa564cd025478fd0b509",
+    "url": "main.47ac0f9df55574b4feb4.css"
+  },
+  {
+    "revision": "17faad8a02b5ebff702a46f485eef03c",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "c16e317f73df8651deaa5ab7a17cf8a5",
+    "url": "manifest.webapp"
+  },
+  {
+    "revision": "6e8da9b3ee0378fd595a2c800af69a55",
+    "url": "content/images/companylogo512.png"
+  },
+  {
+    "url": "app/polyfills.b3912d0426f065bd5588.bundle.js"
+  },
+  {
+    "revision": "271403fbb506af4fc1796009b48c84f9",
+    "url": "content/libraries/xmllint.js/xmllint.js"
+  },
+  {
+    "revision": "662fd0c013e69df2bfcc60ae10258e6c",
+    "url": "content/images/companylogo.svg"
+  },
+  {
+    "revision": "0317843519f81ff18f63d7549166a9b6",
+    "url": "content/json-schema/rsd.1.0.json"
+  },
+  {
+    "revision": "c3cf974679a457a73e7570e8e549b8b3",
+    "url": "content/scss/global.scss"
+  },
+  {
+    "revision": "8b1a84ce1193bbd383528c4ea0e0ba77",
+    "url": "content/scss/vendor.scss"
+  },
+  {
+    "revision": "d682071de30fccf3390e5469285c0a03",
+    "url": "content/xml-schema/v1.0.xml"
+  },
+  {
+    "revision": "53e3f54e348787a62d27",
+    "url": "global.952a3b36beb6dac16508.css"
+  },
+  {
+    "revision": "b7287b2cf0dc904a1fa0d1bb19bdc134",
+    "url": "favicon.ico"
+  },
+  {
+    "revision": "12eb7a25f2c6223ca86c555d662f6e19",
+    "url": "content/css/loading.css"
+  },
+  {
+    "revision": "775aa208b9ea562b9b8bc09bcf94b12b",
+    "url": "content/images/companylogo192.png"
+  },
+  {
+    "revision": "e99b51d0bf5b41097ec891156b7c9de9",
+    "url": "content/libraries/x2js/xml2json.js"
+  },
+  {
+    "revision": "9cad795076a5d371ab04560fc0229a31",
+    "url": "content/images/companylogo384.png"
+  },
+  {
+    "url": "content/662fd0c013e69df2bfcc60ae10258e6c.svg"
+  },
+  {
+    "revision": "aa7b6f13fb32f566daae1f4a897d196b",
+    "url": "content/libraries/x2js/bower.json"
+  },
+  {
+    "revision": "8ee95927336df3616c87da08e7695864",
+    "url": "content/libraries/x2js/package.json"
+  },
+  {
+    "revision": "3a06086a5d0a38c001317167cb37a477",
+    "url": "content/images/close.svg"
+  },
+  {
+    "revision": "ed31deabab707fe7205419f09ec27647",
+    "url": "content/libraries/x2js/check_issues.html"
+  },
+  {
+    "revision": "ff433e3e14a662e039ba65ad31fc2582",
+    "url": "content/libraries/x2js/demo.html"
+  },
+  {
+    "revision": "cdfcd39bf10750b6f60e0c5db009805a",
+    "url": "content/libraries/x2js/xml2json.min.js"
+  },
+  {
+    "revision": "86b3d3cbcdb1ce43ab446c428bd88309",
+    "url": "content/scss/_bootstrap-variables.scss"
+  },
+  {
+    "revision": "dee65c5770122e8ecf168534e2654531",
+    "url": "content/libraries/x2js/samples.html"
+  },
+  {
+    "revision": "9e079d336b17df8cd8b742c628af9774",
+    "url": "content/libraries/x2js/README.md"
+  },
+  {
+    "url": "app/main.b3912d0426f065bd5588.bundle.js"
+  },
+  {
+    "url": "app/global.b3912d0426f065bd5588.bundle.js"
+  },
+  {
+    "url": "app/0.b3912d0426f065bd5588.chunk.js"
+  },
+  {
+    "revision": "edb044d82751cb74900e32f7615771ff",
+    "url": "./i18n/ja.json"
+  },
+  {
+    "revision": "de5e1f3518a739276a358efb71cd1411",
+    "url": "./i18n/en.json"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,9 @@
+importScripts("precache-manifest.b0d531e43f1f62833c52861430dd126a.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+workbox.skipWaiting();
+workbox.clientsClaim();
+
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
